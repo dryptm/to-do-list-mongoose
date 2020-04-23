0779760340c4e9cda0989ec74bba1db403eba70a
Vinaya Kumar <vinay9415756155@gmail.com>
2020-04-24 00:46:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,69 +1,107 @@
-const express=require("express");
-const bodyparser=require("body-parser");
-const mongoose=require("mongoose");
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 
 /*const request=require("request");
 const https=require("https");*/
 const app = express();
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB",{ useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    useNewUrlParser: true
+});
 
-const itemsSchema= {
-    name : String
+const itemsSchema = {
+    name: String
 };
 //items
-const Item=mongoose.model("Item",itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
+const item1=new Item({
+    name:"fucker"
+})
+var defaultitems=[item1]
+
+//customlist
+const listSchema={
+    name:String,
+    items:[itemsSchema]
+}
+const List=mongoose.model("List",listSchema);
 
 
-//workitems
-const workitem=mongoose.model("workitem",itemsSchema);
-
-
-
-
-
-
-
-var worklist=[];
-
-app.get("/",function(req,res)
-{   
-    Item.find({},function(err,founditems){
-        
-        res.render("list",{kindofday:"Today",new_item:founditems})
-    }) 
-});
-
-app.post("/",function(req,res)
-{   
-    if(req.body.list==="work")
-    {   
-        var a=req.body.input;
-        workitem.create({name: a})
-        res.redirect("/work");
-    }
-    else{
-        var a=req.body.input;
-        Item.create({name: a})
-        res.redirect("/");
-    }
-});
-
-app.get("/work",function(req,res)
-{
-    workitem.find({},function(err,workfound)
-    {
-        res.render("list",{kindofday:"work list",new_item:workfound})
+app.get("/:customListname",function(req,res){
+    const customListname=req.params.customListname;
+    console.log(customListname);
+    
+    List.findOne({name :customListname},function(err,foundlist){
+        if(!err){
+            if(foundlist)
+            {
+                console.log("exists")
+                res.render("list", {list_title: foundlist.name,new_item: foundlist.items})
+            }
+            else
+            {   
+                console.log("doesnt exist")
+                const list=new List({
+                    name :customListname,
+                    items:defaultitems
+                })
+                list.save();
+                res.redirect("/"+customListname);
+               // res.render("list",{list_title:customListname,new_item:defaultitems})
+            }
+        }
     })
-    
-    
+})
+
+
+app.get("/", function (req, res) {
+    Item.find({}, function (err, founditems) {
+
+        res.render("list", {
+            list_title: "Today",
+            new_item: founditems
+        })
+    })
+
+   
 });
 
-app.listen(process.env.PORT || 3000,function()
-{
+app.post("/", function (req, res) {
+        var a = req.body.input;
+       
+        const listname=req.body.list;
+        if (listname==="Today")
+            {
+                Item.create({
+                    name: a
+                })
+                
+                res.redirect("/");
+            }
+});
+
+
+app.post("/delete", function (req, res) {
+    const b = req.body.checkbox;
+    Item.findByIdAndRemove(b, function(err)
+    {
+        if (err)
+        {console.log("there's osme error")}
+        else
+        {res.redirect("/")}
+    })
+
+})
+
+
+
+app.listen(process.env.PORT || 3000, function () {
     console.log("server started at 3000");
 });
